00cdaf4b50346844e195dedc4b2f00006cb910c4
DoodleGarbage <redstone48rocks@gmail.com>
2025-04-16 10:09:01 -0700

    This was nothing but pain and I still don't know why it works

--- LocalRespawnsUpdated/LocalRespawnsUpdatedModSystem.cs
+++ LocalRespawnsUpdated/LocalRespawnsUpdatedModSystem.cs
@@ -5,6 +5,7 @@ using Vintagestory.API.Server;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
+using System.Linq.Expressions;
 
 
 namespace LocalRespawns;
@@ -40,7 +41,7 @@ public class LocalRespawnsModSystem : ModSystem
         _sapi = api;
         api.Event.OnEntityDeath += OnEntityDeath;
         api.Event.PlayerRespawn += OnPlayerRespawn;
-        
+        api.Event.ChunkColumnLoaded += OnChunkLoaded;
 
     }
 
@@ -59,7 +60,7 @@ public class LocalRespawnsModSystem : ModSystem
         playerData.position = byPlayer.GetSpawnPosition(true).XYZInt;
 
         lastSpawnLocations.Add(playerData);
-        
+        //Mod.Logger.Notification("Uh oh! Player died! Trying to find floor!");
         var loc = byPlayer.Entity.ServerPos;
         Vec3i newLocation = GenerateSpawnLocation(loc);
         byPlayer.ClearSpawnPosition();
@@ -70,17 +71,52 @@ public class LocalRespawnsModSystem : ModSystem
     {
         for (int i = 0; i < lastSpawnLocations.Count; i++)
         {
-            if (lastSpawnLocations[i].player == byPlayer)
-            {
-                
+            if (byPlayer == lastSpawnLocations[i].player) {
+                BlockPos surface_block = findFloor(byPlayer.Entity.ServerPos.AsBlockPos);
+                if (surface_block == new BlockPos(0)) {
+                    break;
+                }
+                Vec3d surf_block_double = surface_block.ToVec3d();
+                byPlayer.Entity.TeleportToDouble(surf_block_double.X, surf_block_double.Y, surf_block_double.Z);
                 PlayerSpawnPos loc = new PlayerSpawnPos(lastSpawnLocations[i].position.X, lastSpawnLocations[i].position.Y, lastSpawnLocations[i].position.Z);
-                byPlayer.SetSpawnPosition(loc);
+                lastSpawnLocations[i].player.SetSpawnPosition(loc);
                 lastSpawnLocations.RemoveAt(i);
-                return;
+                break;
             }
         }
     }
 
+    private void OnChunkLoaded(Vec2i chunkCoord, IWorldChunk[] chunks)
+    {
+        List<int> to_delete = new List<int>();
+        for (int i = 0; i < lastSpawnLocations.Count; i++)
+        {
+            if (lastSpawnLocations[i].player.Entity.ServerPos.SquareDistanceTo(lastSpawnLocations[i].player.GetSpawnPosition(false)) < 0.25f) 
+            {
+                //Mod.Logger.Notification("Tried to teleport the player!");
+                // Teleport the player to the ground, to counteract spawning in the ground.
+                BlockPos surface_block = findFloor(lastSpawnLocations[i].player.Entity.ServerPos.AsBlockPos);
+                if (surface_block == new BlockPos(0)){
+                    //Mod.Logger.Notification("Failed to find floor. Try again later.");
+                    continue;
+                }
+                Vec3d surf_block_double = surface_block.ToVec3d();
+                lastSpawnLocations[i].player.Entity.TeleportToDouble(surf_block_double.X, surf_block_double.Y, surf_block_double.Z);
+                PlayerSpawnPos loc = new PlayerSpawnPos(lastSpawnLocations[i].position.X, lastSpawnLocations[i].position.Y, lastSpawnLocations[i].position.Z);
+                lastSpawnLocations[i].player.SetSpawnPosition(loc);
+                to_delete.Append(i);
+                break;
+             }
+        }
+        for (int i = to_delete.Count-1; i >= 0; i--)
+        {
+            lastSpawnLocations.RemoveAt(i);
+        }
+        
+
+
+        
+    }
     private Vec3i GenerateSpawnLocation(EntityPos pos)
     {
         Random rnd = new Random();
@@ -92,6 +128,12 @@ public class LocalRespawnsModSystem : ModSystem
         NewPosition.X += randx;
         NewPosition.Z += randz;
         BlockPos floorPos = findFloor(NewPosition.AsBlockPos);
+        if (floorPos == new BlockPos(0)) {
+            floorPos.X = NewPosition.AsBlockPos.X;
+            floorPos.Z = NewPosition.AsBlockPos.Z;
+            floorPos.Y = 100;
+        }
+
 
         Vec3i newPos = floorPos.ToVec3i();
         
@@ -104,15 +146,19 @@ public class LocalRespawnsModSystem : ModSystem
 
         for (int i = worldHeight - 1; i > 0; i--)
         {
+
             floorPos.Set(origin.X, i, origin.Z);
             var block = _sapi.World.BlockAccessor.GetBlock(floorPos);
+            //Mod.Logger.Notification($"Checking Block; Location: {floorPos}");
             if (block.BlockId != 0 && block.CollisionBoxes != null)
             {
+                //Mod.Logger.Notification("Successfully found a floor!");
                 floorPos.Set(origin.X, i + 1, origin.Z);
                 return floorPos;
             }
         }
         // Should only call when there is all air.
-        return origin.Copy();
+        //Mod.Logger.Notification("Uh oh! Returning origin!");
+        return new BlockPos(0);
     }
 }
